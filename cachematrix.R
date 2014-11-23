959d825e6ba0b2d8504587e3579a2f32029a7041
Christian Gonzalez <cgonzalez@wovenware.com>
2014-11-22 23:54:33 -0400

    Changed comments ...

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## These functions create a matrix and calculates the inverse of that matrix. 
-## The inverse matrix, once calulated is cached and stored in the parent
+## The inverse matrix, once calulated, is cached and stored in the parent
 ## environment.  If the inverse of that matrix has been calculated before, the 
-## inverse matrix is not calculated again.
+## inverse matrix is not calculated again and the cached version is returned.
 
-## makeCacheMatrix: This funcion creates a defaul empty matrix.  It provides
-## methods to set a matrix (set), get a matrix (get), set an inverse matrix 
-## (setInverse), get the inverse matrix (getInverse).
+## makeCacheMatrix: This funcion creates a defaul empty matrix or creates one 
+## with the the argument.  It provides methods to set a new matrix (set), get 
+## the set matrix (get), set an inverse matrix (setInverse), get the inverse matrix (getInverse).
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -27,12 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-## cacheSolve:  This function calculates the inverse matrix of x, if the
-## inverse matrix hasn't been calculated before and cached in the context 
-## of makeCacheMatrix
+## cacheSolve: This function calculates the inverse of matrix retrieved from x.
+## If the inverse matrix retrieved from x is NULL, the inverse matrix is 
+## is calculated.  If not, the inverse matrix is retrieved from the parent 
+## environment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   i <- x$getInverse();
   
   if(!is.null(i)) {
